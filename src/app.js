0de44f74d8250a9436ad8d7548276e7b8056ea1d
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-02-28 20:04:49 +0000

    Extension One Complete

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ function App() {
   // Use initialEmails for state
   const [listOfEmails, setEmails] = useState(initialEmails)
   console.log(initialEmails)
+  //const [showReadEmails, setShowReadEmails] = useState(setCompletedtrue)
+
 
 const emails=listOfEmails.map(renderEmails => {
   return <li className="email">
@@ -28,10 +30,33 @@ const emails=listOfEmails.map(renderEmails => {
   <div className="title">{renderEmails.title}</div>
 </li>
 })
-
+//Create a toggleRead function that updates the target email's read property in state, when a user clicks on the checkbox
 const toggleRead = (target) => {
-
+  console.log("bye", updatedEmails)
+  const updatedEmails = listOfEmails.map(email => 
+    email === target ? {...email, read: !email.read} : email)
+    console.log("byee", updatedEmails)
+    setEmails(updatedEmails)
 }
+
+// const completeWorkout = (target) => {
+//   const doneWorkouts = workouts.map(workout =>
+//     workout === target ? { ...workout, done: !false } : workout
+//   )
+//   setWorkouts(doneWorkouts)
+// }
+
+// Create a toggleStar function that updates the target email's starred property in state, when a user clicks on the star
+const toggleStar = (target) => {
+  console.log("hi", updatedEmails)
+  const updatedEmails = listOfEmails.map(email => 
+    email === target ? {...email, starred: !email.starred} : email)
+    console.log("hello", updatedEmails)
+    setEmails(updatedEmails)
+}
+
+// const incompleteTodos = listOfEmails.filter(email => !email.completed)
+//   const completedTodos = listOfEmails.filter(email => email.completed)
 
   return (
     <div className="app">
@@ -40,7 +65,7 @@ const toggleRead = (target) => {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+             //onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
@@ -59,7 +84,7 @@ const toggleRead = (target) => {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+              //onChange={() => {}}
             />
           </li>
         </ul>
